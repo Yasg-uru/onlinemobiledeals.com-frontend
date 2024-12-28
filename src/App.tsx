55d08883732ce7cc18c9367eb70976bf0728b10b
yasg-uru <yashpawar12122004@gmail.com>
2024-12-28 21:18:13 +0530

    created protect route in add product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
+import "./App.css";
+import AddProductPage from "./pages/createproduct";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./pages/navbar";
+import LoginPage from "./pages/Login";
 
-import './App.css'
-import AddProductPage from './pages/createproduct'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Navbar } from './pages/navbar'
-import LoginPage from './pages/Login'
-
-import Products from './pages/product'
-import { UpdateProductPage } from './pages/updateproduct'
+import Products from "./pages/product";
+import { UpdateProductPage } from "./pages/updateproduct";
+import { useAuthContext } from "./context/authcontext";
 
 function App() {
- 
-
+  const { user, isAuthenticated } = useAuthContext();
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>  
-      <Route path='/add-product' element={<AddProductPage/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/' element={<Products/>}/>
-      <Route path='/update-product/:productId' element={<UpdateProductPage/>} />
+      <Navbar />
+      <Routes>
+        {user && isAuthenticated && user.Role === "admin" && (
+          <Route path="/add-product" element={<AddProductPage />} />
+        )}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Products />} />
+        <Route
+          path="/update-product/:productId"
+          element={<UpdateProductPage />}
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
